da486f73973d10c5c7b640940141c061fc18a8b4
Kevin Gann <kgann@5amsolutions.com>
2015-12-14 11:46:02 +0530

    Extract method. Move variables.

--- test_slap_kirk.py
+++ test_slap_kirk.py
@@ -9,13 +9,13 @@ import unittest
 TARGET_URL = 'http://www.slapkirk.com/play'
 WAIT_SPOCK_HAND = (By.CSS_SELECTOR, 'fa-hand-spock-o')
 WAIT_SPOCK_HAND_SPINNER = (By.CSS_SELECTOR, 'fa-spin')
-ANIMATION_IMAGE = (By.ID, 'animationImage')
+KIRK_IMAGE = (By.ID, 'animationImage')
 SCORE_BOX = (By.ID, 'scoreBox')
-Y_OFFSET = 100
 
 WAIT = 30
 SLAP_COUNT = 1000
 OFFSET_INCREMENT = 30
+Y_OFFSET = 100
 BROWSER_WINDOW_HEIGHT = 600
 BROWSER_WINDOW_WIDTH = 875
 
@@ -26,9 +26,7 @@ class SlapKirkTest(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.set_window_size(BROWSER_WINDOW_WIDTH, BROWSER_WINDOW_HEIGHT)
         self.driver.get(TARGET_URL)
-        self.wait_for_something(EC.invisibility_of_element_located(WAIT_SPOCK_HAND))
-        self.wait_for_something(EC.invisibility_of_element_located(WAIT_SPOCK_HAND_SPINNER))
-        self.wait_for_something(EC.presence_of_element_located(ANIMATION_IMAGE))
+        self.wait_for_loading()
 
     def tearDown(self):
         self.driver.quit()
@@ -59,8 +57,13 @@ class SlapKirkTest(unittest.TestCase):
         except MoveTargetOutOfBoundsException:
             print("Tried to move to offset but couldn't!")
 
+    def wait_for_loading(self):
+        self.wait_for_something(EC.invisibility_of_element_located(WAIT_SPOCK_HAND))
+        self.wait_for_something(EC.invisibility_of_element_located(WAIT_SPOCK_HAND_SPINNER))
+        self.wait_for_something(EC.presence_of_element_located(KIRK_IMAGE))
+
     def wait_for_kirk_image(self):
-        kirk_image = self.wait_for_something(EC.presence_of_element_located(ANIMATION_IMAGE))
+        kirk_image = self.wait_for_something(EC.presence_of_element_located(KIRK_IMAGE))
         return kirk_image
 
     def wait_for_something(self, expected_condition):
